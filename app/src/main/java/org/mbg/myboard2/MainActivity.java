af6d4d9099e26304425e12772637c53abe620d05
wonawonana <56299516+wonawonana@users.noreply.github.com>
2020-09-19 03:06:46 +0900

    MainActivity logout

--- app/src/main/java/org/mbg/myboard2/MainActivity.java
+++ app/src/main/java/org/mbg/myboard2/MainActivity.java
@@ -2,7 +2,10 @@ package org.mbg.myboard2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.firebase.auth.FirebaseAuth;
 
@@ -13,12 +16,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button logoutButton=findViewById(R.id.button_logout);
+        logoutButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                signOut();
+            }});
     }
+
 
     public void signOut() {
         // [START auth_sign_out]
         FirebaseAuth.getInstance().signOut();
         // [END auth_sign_out]
+
+        //로그아웃 하면 로그인 화면으로 돌아감
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+        finish();
     }
 
 }
